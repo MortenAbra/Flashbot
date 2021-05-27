53711062cc4a602dc2a484bff2b694fe42959d3f
MortenAbra <sezure@gmail.com>
2021-05-27 23:25:35 +0200

    Moved files and folder

--- app/scripts/app.js
+++
@@ -1,8 +0,0 @@
-var name = new Vue({
-    el: '#name',
-    data: {
-        authorName: 'Morten Abrahamsen Olesen',
-        authorEducation: 'Cand.polyt - Software Engineering',
-        authorAge: "26"
-    }
-})
