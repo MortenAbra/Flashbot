33af5949fa19b97635a6f2207d2acce709f25b41
MortenAbra <sezure@gmail.com>
2021-05-27 23:24:10 +0200

    Extended website design

---
+++ app/scripts/app.js
@@ -0,0 +1,8 @@
+var name = new Vue({
+    el: '#name',
+    data: {
+        authorName: 'Morten Abrahamsen Olesen',
+        authorEducation: 'Cand.polyt - Software Engineering',
+        authorAge: "26"
+    }
+})
